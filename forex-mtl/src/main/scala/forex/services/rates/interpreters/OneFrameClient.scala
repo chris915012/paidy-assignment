72644d84ebfa638516427e3c2ab53c3a888fb86d
Chris <dev.chris.kr@gmail.com>
2022-12-17 14:07:47 +0900

    (feat) Logging for OneFrameClient
    - Add exception block for the client
    - Add error logging on calling the cleint

--- forex-mtl/src/main/scala/forex/services/rates/interpreters/OneFrameClient.scala
+++ forex-mtl/src/main/scala/forex/services/rates/interpreters/OneFrameClient.scala
@@ -3,14 +3,21 @@ package forex.services.rates.interpreters
 import cats.Applicative
 import cats.syntax.applicative._
 import cats.syntax.either._
+import ch.qos.logback.classic.LoggerContext
+import ch.qos.logback.core.util.StatusPrinter
 import com.typesafe.config.ConfigFactory
 import forex.domain.{Price, Rate, Timestamp}
 import forex.services.rates.Algebra
 import forex.services.rates.errors._
-import org.json4s.DefaultFormats
+import org.json4s.{DefaultFormats, JValue}
 import org.json4s.jackson.JsonMethods._
+import org.slf4j.{Logger, LoggerFactory}
 
 class OneFrameClient[F[_]: Applicative] extends Algebra[F] {
+
+  def logger: Logger = LoggerFactory.getLogger("OneFrame")
+  StatusPrinter.print((LoggerFactory.getILoggerFactory).asInstanceOf[LoggerContext])
+
   override def get(pair: Rate.Pair): F[Error Either Rate] = {
 
     val config = ConfigFactory.load("application.conf").getConfig("app")
@@ -21,11 +28,19 @@ class OneFrameClient[F[_]: Applicative] extends Algebra[F] {
       headers = Map("token" -> oneFrameConfig.getString("token"))
     )
 
-    val uri = oneFrameConfig.getString("address") + s"/rates?pair=${pair.from}${pair.to}"
-    val parsedJson = parse(session.get(uri).text())
-    val price = (parsedJson \ "price").extract[List[Double]]
+    var jsonString: String = ""
+    var uri: String = ""
 
+    try {
+      uri = oneFrameConfig.getString("address") + s"/rates?pair=${pair.from}${pair.to}"
+      jsonString = session.get(uri).text()
+    } catch {
+      case _: Exception => logger.error("ERROR: " + uri).pure[F]
+    }
+    val parsedJson: JValue = parse(jsonString)
+    val price = (parsedJson \ "price").extract[List[Double]]
     val rate = Rate(pair, Price(price.head), Timestamp.now)
     rate.asRight[Error].pure[F]
+
   }
 }
