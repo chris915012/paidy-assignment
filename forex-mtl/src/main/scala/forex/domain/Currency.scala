3dfdc2f0167895662272f87a9ee513b0971ee6bb
Chris <dev.chris.kr@gmail.com>
2022-12-17 22:46:21 +0900

    (refactor) Refactor the Currency class

--- forex-mtl/src/main/scala/forex/domain/Currency.scala
+++ forex-mtl/src/main/scala/forex/domain/Currency.scala
@@ -1,31 +1,30 @@
 package forex.domain
 
 import cats.Show
+import cats.syntax.show._
+import io.circe.Json
+import io.circe.Encoder
 
-sealed trait Currency
+sealed trait Currency extends Product with Serializable {
+  def code: String
+}
 
 object Currency {
-  case object AUD extends Currency
-  case object CAD extends Currency
-  case object CHF extends Currency
-  case object EUR extends Currency
-  case object GBP extends Currency
-  case object NZD extends Currency
-  case object JPY extends Currency
-  case object SGD extends Currency
-  case object USD extends Currency
+  case object AUD extends Currency { val code: String = "AUD" }
+  case object CAD extends Currency { val code: String = "CAD" }
+  case object CHF extends Currency { val code: String = "CHF" }
+  case object EUR extends Currency { val code: String = "EUR" }
+  case object GBP extends Currency { val code: String = "GBP" }
+  case object NZD extends Currency { val code: String = "NZD" }
+  case object JPY extends Currency { val code: String = "JPY" }
+  case object SGD extends Currency { val code: String = "SGD" }
+  case object USD extends Currency { val code: String = "USD" }
 
-  implicit val show: Show[Currency] = Show.show {
-    case AUD => "AUD"
-    case CAD => "CAD"
-    case CHF => "CHF"
-    case EUR => "EUR"
-    case GBP => "GBP"
-    case NZD => "NZD"
-    case JPY => "JPY"
-    case SGD => "SGD"
-    case USD => "USD"
-  }
+  val all: Set[Currency] = Set(AUD, CAD, CHF, EUR, GBP, NZD, JPY, SGD, USD)
+
+  implicit val show: Show[Currency] = _.code.show
+  implicit val encoder: Encoder[Currency] =
+    Encoder.instance[Currency] { c => Json.fromString(c.code) }
 
   def fromString(s: String): Currency = s.toUpperCase match {
     case "AUD" => AUD
