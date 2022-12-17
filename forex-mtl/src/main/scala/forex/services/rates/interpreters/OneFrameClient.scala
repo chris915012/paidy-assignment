1e78c526b2bb48e7d6c034347d04301edd0044da
Chris <44944612+chdlsp@users.noreply.github.com>
2022-12-17 13:21:59 +0900

    (feat) Refactor the OneFrameClient
    - Set a constant using a application.conf
    - Set variables into the URI to call OneFrame

--- forex-mtl/src/main/scala/forex/services/rates/interpreters/OneFrameClient.scala
+++ forex-mtl/src/main/scala/forex/services/rates/interpreters/OneFrameClient.scala
@@ -3,6 +3,7 @@ package forex.services.rates.interpreters
 import cats.Applicative
 import cats.syntax.applicative._
 import cats.syntax.either._
+import com.typesafe.config.ConfigFactory
 import forex.domain.{Price, Rate, Timestamp}
 import forex.services.rates.Algebra
 import forex.services.rates.errors._
@@ -11,16 +12,20 @@ import org.json4s.jackson.JsonMethods._
 
 class OneFrameClient[F[_]: Applicative] extends Algebra[F] {
   override def get(pair: Rate.Pair): F[Error Either Rate] = {
-    implicit val formats = DefaultFormats
-    val session = requests.Session(
-      headers = Map("token" -> "10dc303535874aeccc86a8251e6992f5")
-    )
-    val response = session.get("http://localhost:8080/rates?pair=USDJPY")
-    val responseText = response.text()
-    val parsedJson = parse(responseText)
-    val value1 = (parsedJson \ "price").extract[List[Double]]
 
-    val rate = Rate(pair, Price(value1.head), Timestamp.now)
+    val config = ConfigFactory.load("application.conf").getConfig("app")
+    val oneFrameConfig = config.getConfig("one-frame")
+
+    implicit val formats: DefaultFormats.type = DefaultFormats
+    val session = requests.Session(
+      headers = Map("token" -> oneFrameConfig.getString("token"))
+    )
+
+    val uri = oneFrameConfig.getString("address") + s"/rates?pair=${pair.from}${pair.to}"
+    val parsedJson = parse(session.get(uri).text())
+    val price = (parsedJson \ "price").extract[List[Double]]
+
+    val rate = Rate(pair, Price(price.head), Timestamp.now)
     rate.asRight[Error].pure[F]
   }
 }
