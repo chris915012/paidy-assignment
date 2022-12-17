02cdffbbcc546ad3db9580c2b3a8baac3f915683
Chris <dev.chris.kr@gmail.com>
2022-12-17 16:22:07 +0900

    (refactor) Refactor the OneFrameClient
    - Replaced the try-catch block with the Either.catchNonFatal method
    - Replaced the Error code with existing code

--- forex-mtl/src/main/scala/forex/services/rates/interpreters/OneFrameClient.scala
+++ forex-mtl/src/main/scala/forex/services/rates/interpreters/OneFrameClient.scala
@@ -8,18 +8,17 @@ import ch.qos.logback.core.util.StatusPrinter
 import com.typesafe.config.ConfigFactory
 import forex.domain.{Price, Rate, Timestamp}
 import forex.services.rates.Algebra
+import forex.services.rates.errors.Error.OneFrameLookupFailed
 import forex.services.rates.errors._
-import org.json4s.{DefaultFormats, JValue}
 import org.json4s.jackson.JsonMethods._
-import org.slf4j.{Logger, LoggerFactory}
+import org.json4s.{DefaultFormats, JValue}
+import org.slf4j.LoggerFactory
 
 class OneFrameClient[F[_]: Applicative] extends Algebra[F] {
 
-  def logger: Logger = LoggerFactory.getLogger("OneFrame")
   StatusPrinter.print((LoggerFactory.getILoggerFactory).asInstanceOf[LoggerContext])
 
   override def get(pair: Rate.Pair): F[Error Either Rate] = {
-
     val config = ConfigFactory.load("application.conf").getConfig("app")
     val oneFrameConfig = config.getConfig("one-frame")
 
@@ -28,19 +27,15 @@ class OneFrameClient[F[_]: Applicative] extends Algebra[F] {
       headers = Map("token" -> oneFrameConfig.getString("token"))
     )
 
-    var jsonString: String = ""
-    var uri: String = ""
-
-    try {
-      uri = oneFrameConfig.getString("address") + s"/rates?pair=${pair.from}${pair.to}"
-      jsonString = session.get(uri).text()
-    } catch {
-      case _: Exception => logger.error("ERROR: " + uri).pure[F]
-    }
-    val parsedJson: JValue = parse(jsonString)
-    val price = (parsedJson \ "price").extract[List[Double]]
-    val rate = Rate(pair, Price(price.head), Timestamp.now)
-    rate.asRight[Error].pure[F]
-
+    val uri: String = oneFrameConfig.getString("address") + s"/rates?pair=${pair.from}${pair.to}"
+    val eitherJsonString: Error Either String = Either.catchNonFatal(session.get(uri).text())
+      .leftMap(e => OneFrameLookupFailed(e.getMessage))
+    eitherJsonString.flatMap { jsonString =>
+      val eitherParsedJson: Error Either JValue = Either.catchNonFatal(parse(jsonString)).leftMap(e => OneFrameLookupFailed(e.getMessage))
+      eitherParsedJson.map { parsedJson =>
+        val price = (parsedJson \ "price").extract[List[Double]]
+        Rate(pair, Price(price.head), Timestamp.now)
+      }
+    }.pure[F]
   }
 }
